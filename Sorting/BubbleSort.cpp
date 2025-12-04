8a11955a19019944b22c20b50a5b2ba3ba07cdfa
devDaviHtrb <davideleteribeiro1@gmail.com>
2025-12-04 01:20:51 -0300

    <add> common and recursive bubble sort

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bubbleSort(vector<int> array, int end)
+{
+    if (end == 1)
+        return array;
+
+    for (int i = 0; i < end - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+            swap(array[i], array[i + 1]);
+    }
+
+    return bubbleSort(array, end - 1);
+}
+
+vector<int> recursiveBubbleSort(vector<int> &array, int end, int current_index)
+{
+    if (end == 1)
+        return array;
+    if (current_index < end - 1)
+    {
+        if (array[current_index] > array[current_index + 1])
+        {
+            swap(array[current_index], array[current_index + 1]);
+        }
+        return recursiveBubbleSort(array, end, current_index + 1);
+    }
+    return recursiveBubbleSort(array, end - 1, 0);
+}
+int main()
+{
+    return 0;
+}
